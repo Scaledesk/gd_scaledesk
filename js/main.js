866d762e539219541a8989aa71c32aa1a1a67121
sanchitrathee <sanchit2411@gmail.com>
2017-02-14 17:39:06 +0530

    add category page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $('li.dropdown').click(function(){
 	$('this').addClass('open').find('ul.dropdown-menu').animate({top:35},200)
 
 })
-
+function LoadImage(){
+	$(this).append('<img src="./img/slide-1.jpg" alt=""/>');
+	$(this).remove();
+}
